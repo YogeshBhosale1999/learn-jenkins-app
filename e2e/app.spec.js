56101fbe7124c244f78fdc6844754350ecaa5f0e
Yogesh Jalindar Bhosale <65329871+YogeshBhosale1999@users.noreply.github.com>
2026-04-04 13:48:39 +0000

    made changes to appsec

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -2,11 +2,9 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
-  // Use Jenkins-provided URL if available, otherwise default to local root
   const targetUrl = process.env.CI_ENVIRONMENT_URL || 'http://localhost:3000';
   await page.goto(targetUrl);
 
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Learn Jenkins/);
 });
 
@@ -23,7 +21,6 @@ test('has expected app version', async ({ page }) => {
   await page.goto(targetUrl);
 
   const expectedAppVersion = process.env.REACT_APP_VERSION ? process.env.REACT_APP_VERSION : '1';
-
   console.log(expectedAppVersion);
 
   const isVisible = await page.locator(`p:has-text("Application version: ${expectedAppVersion}")`).isVisible();
