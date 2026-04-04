50d0baf0a70bd5949b477667f7e635fd7f50abc3
Yogesh Jalindar Bhosale <65329871+YogeshBhosale1999@users.noreply.github.com>
2026-04-04 13:57:44 +0000

    reverted appsec and jenkins

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -2,25 +2,24 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
-  const targetUrl = process.env.CI_ENVIRONMENT_URL || 'http://localhost:3000';
-  await page.goto(targetUrl);
+  await page.goto('/');
 
+  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Learn Jenkins/);
 });
 
 test('has Jenkins in the body', async ({ page }) => {
-  const targetUrl = process.env.CI_ENVIRONMENT_URL || 'http://localhost:3000';
-  await page.goto(targetUrl);
+  await page.goto('/');
 
   const isVisible = await page.locator('a:has-text("Learn Jenkins on Udemy")').isVisible();
   expect(isVisible).toBeTruthy();
 });
 
 test('has expected app version', async ({ page }) => {
-  const targetUrl = process.env.CI_ENVIRONMENT_URL || 'http://localhost:3000';
-  await page.goto(targetUrl);
+  await page.goto('/');
 
   const expectedAppVersion = process.env.REACT_APP_VERSION ? process.env.REACT_APP_VERSION : '1';
+
   console.log(expectedAppVersion);
 
   const isVisible = await page.locator(`p:has-text("Application version: ${expectedAppVersion}")`).isVisible();
